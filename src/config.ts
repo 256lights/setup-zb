90b5b1cfb085ac0639603fbfb7857b903aca7019
Roxy Light <roxy@256lights.llc>
2026-08-03 15:56:22 -0700

    Add inputs for discovery documents

--- src/config.ts
+++ src/config.ts
@@ -4,40 +4,96 @@ import path from 'node:path';
 
 import * as core from '@actions/core';
 
+interface ZBConfiguration {
+  server?: ZBServerConfiguration;
+}
+
+interface ZBServerConfiguration {
+  download?: StoreLocator;
+  upload?: StoreLocator;
+  signingKeyFiles?: string[];
+}
+
+type StoreLocator = NullStoreLocator | HTTPStoreLocator;
+
+type NullStoreLocator = null | {type: 'null'};
+
+interface HTTPStoreLocator {
+  type: 'http';
+  url: string;
+}
+
 export interface ConfigurationInputs {
   'configuration': string;
   'signing-key': string;
+  'server-download-discovery': string;
+  'server-upload-discovery': string;
 }
 
 export function getConfigurationInputs(): ConfigurationInputs {
-  return {
-    configuration: core.getInput('configuration'),
-    'signing-key': core.getInput('signing-key'),
+  const inputs: Partial<ConfigurationInputs> = {};
+  const keys: (keyof ConfigurationInputs)[] = [
+    'configuration',
+    'signing-key',
+    'server-download-discovery',
+    'server-upload-discovery',
+  ];
+  for (const key of keys) {
+    inputs[key] = core.getInput(key);
   }
+  return inputs as ConfigurationInputs;
+}
+
+function hasGenerated(inputs: ConfigurationInputs): boolean {
+  return !!inputs['signing-key'] ||
+    !!inputs['server-download-discovery'] ||
+    !!inputs['server-upload-discovery'];
 }
 
 /**
  * @returns A list of configuration files in descending precedence order.
  */
 export async function createConfigurationFiles(inputs: ConfigurationInputs): Promise<string[]> {
-  if (!inputs.configuration && !inputs['signing-key']) {
+  if (!inputs.configuration && !hasGenerated(inputs)) {
     return [];
   }
 
   const configFiles = [];
   const configDir = await fs.mkdtemp(path.join(tmpdir(), 'zb-config-'));
 
-  if (inputs['signing-key']) {
-    const keyPath = path.join(configDir, 'signing-key.json');
-    await fs.writeFile(keyPath, inputs['signing-key'], {
-      encoding: 'utf-8',
-    });
+  if (hasGenerated(inputs)) {
+    const serverConfig: ZBServerConfiguration = {};
+
+    if (inputs['signing-key']) {
+      const keyPath = path.join(configDir, 'signing-key.json');
+      await fs.writeFile(keyPath, inputs['signing-key'], {
+        encoding: 'utf-8',
+      });
+      serverConfig.signingKeyFiles = ['signing-key.json'];
+    }
+    if (inputs['server-download-discovery']) {
+      const keyPath = path.join(configDir, 'download-discovery.json');
+      await fs.writeFile(keyPath, inputs['server-download-discovery'], {
+        encoding: 'utf-8',
+      });
+      serverConfig.download = {
+        type: 'http',
+        url: 'download-discovery.json',
+      };
+    }
+    if (inputs['server-upload-discovery']) {
+      const keyPath = path.join(configDir, 'upload-discovery.json');
+      await fs.writeFile(keyPath, inputs['server-upload-discovery'], {
+        encoding: 'utf-8',
+      });
+      serverConfig.upload = {
+        type: 'http',
+        url: 'upload-discovery.json',
+      };
+    }
+
+    const configObject: ZBConfiguration = {server: serverConfig};
     const p = path.join(configDir, 'action.json');
-    const configObject = {
-      server: {
-        signingKeyFiles: ['signing-key.json'],
-      }
-    };
     await fs.writeFile(p, JSON.stringify(configObject), {
       encoding: 'utf-8',
     });
